166da12ff4d004f5f59da1b49de3afc3818551e1
Raphael Jardim Lopes <raphaeljlps@users.noreply.github.com>
2014-10-24 15:33:58 -0200

    Update statistics.go

--- statistics/statistics.go
+++ statistics/statistics.go
@@ -7,7 +7,7 @@ import "errors"
 ///////////////////////////////////////////////////
 
 //Stat is a struct that have the two input values
-//used in the calc operations
+//used in the calc operations.
 type Stat struct {
 	Lambda float64
 	Mu     float64
@@ -18,8 +18,8 @@ type Stat struct {
 ///////////////////////////////////////////////////
 
 //TrafficIntensity calculate the minimum amount of servers
-//needed to maintain the incoming of jobs to que queue
-//Queue M/M/1
+//needed to maintain the incoming of jobs to que queue.
+//Queue type M/M/1
 func (stat Stat) TrafficIntensity() (result float64, err error) {
 	if stat.Mu == 0 {
 		return 0.0, errors.New("Can't divide by zero")
