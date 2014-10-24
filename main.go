4019256242a819cf3ab8a05a30b580f172397a0c
Raphael Jardim Lopes <raphaeljlps@users.noreply.github.com>
2014-10-24 14:29:59 -0200

    Update main.go
    Addition of cli lib to facilitate cli ops

--- main.go
+++ main.go
@@ -1,32 +1,69 @@
+//////////////////////////////////////////////////
+//             author: Raphael Jardim Lopes     //
+//             all rights reserved              //
+//             date: 24/10/2014                 //
+//////////////////////////////////////////////////
+
 package main
 
 import (
-	"flag"
 	"fmt"
+	"os"
+
+	"github.com/codegangsta/cli"
 )
 
+///////////////////////////////////////////////////
+//                Structs                        //
+///////////////////////////////////////////////////
 type statistic struct {
 	lambda float64
 	mu     float64
 }
 
-var l, m *float64
-
-func init() {
-	l = flag.Float64("l", 0.0, "lambda")
-	m = flag.Float64("m", 0.0, "Mu")
-	
-}
-
+///////////////////////////////////////////////////
+//                Main function                  //
+///////////////////////////////////////////////////
 func main() {
-        flag.Parse()
-	stats := statistic{*l, *m}
+	app := cli.NewApp()
+	app.Name = "Queue Modeling Tool"
+	app.Usage = "Gives a list with the formulas and answers with the given λ and μ"
 
-	fmt.Printf("%s", stats.trafficIntensity())
+	//flags
+	app.Flags = []cli.Flag{
+		cli.Float64Flag{
+			Name:  "lambda",
+			Value: 0.0,
+			Usage: "lambda measured",
+		},
+		cli.Float64Flag{
+			Name:  "mu",
+			Value: 1.0,
+			Usage: "mu measured",
+		},
+	}
+
+	//actions
+	app.Action = func(c *cli.Context) {
+
+		l := c.Float64("lambda")
+		m := c.Float64("mu")
+
+		stats := statistic{lambda: l, mu: m}
+
+		fmt.Printf("%s", stats.trafficIntensity())
+
+	}
+	app.Run(os.Args)
 }
+
+///////////////////////////////////////////////////
+//               Calculation functions           //
+///////////////////////////////////////////////////
 
 func (stats statistic) trafficIntensity() (result string) {
 	rho := stats.lambda / stats.mu
-	fmt.Sprintf(result, "ρ = λ / μ = %5.2f", rho)
-	return
+	result = fmt.Sprintf("ρ = λ / μ = %5.2f", rho)
+	return result
 }
+
