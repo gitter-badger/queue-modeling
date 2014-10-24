04b8771fee0f439fd00842ac85d1e8881ba6ded8
Raphael Jardim Lopes <raphaeljlps@users.noreply.github.com>
2014-10-24 15:23:55 -0200

    Create statistics_test.go

---
+++ statistics/statistics_test.go
@@ -0,0 +1,38 @@
+package statistics
+
+import "testing"
+
+func TestTrafficIntensityOK(t *testing.T) {
+	stat := Stat{Lambda: 10, Mu: 10}
+
+	result, _ := stat.TrafficIntensity()
+	value := 1.0
+	if result != value {
+		t.Error("For {Lambda: ", stat.Lambda, " Mu: ", stat.Mu, "} ",
+			"Expected ", value,
+			"Got ", result)
+	}
+
+	stat.Lambda = 20
+
+	result, _ = stat.TrafficIntensity()
+	value = 2.0
+
+	if result != value {
+		t.Error("For {Lambda: ", stat.Lambda, " Mu: ", stat.Mu, "} ",
+			"Expected ", value,
+			"Got ", result)
+	}
+}
+
+func TestTrafficIntensityError(t *testing.T)  {
+  stat := Stat{Lambda: 10, Mu: 0}
+
+  _, err := stat.TrafficIntensity()
+
+  if err == nil {
+    t.Error("For {Lambda: ", stat.Lambda, " Mu: ", stat.Mu, "} ",
+      "Expected error not nil",
+            "Got error nil")
+  }
+}
