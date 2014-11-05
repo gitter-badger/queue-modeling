b01e08bc7c86c4500c690cf34cdc59faf81f102c
Raphael Jardim Lopes <raphaeljlps@users.noreply.github.com>
2014-11-05 12:23:11 -0200

    Update statistics.go

--- statistics/statistics.go
+++ statistics/statistics.go
@@ -38,7 +38,7 @@ func (stat Stat) TrafficIntensity() (result float64, err error) {
 func (stat Stat) ZeroJobsInSystem() (result float64, err error) {
 	p, er := stat.TrafficIntensity()
 	if er != nil {
-		return 0, err
+		return 0, er
 	}
 
 	p0 := 1 - p
